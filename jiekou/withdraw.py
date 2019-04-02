fbdecfe15fb7996ff486ae7a1041ab918f2d1bb1
luren <2505181095@qq.com>
2019-04-02 14:53:17 +0800

    1

---
+++ jiekou/withdraw.py
@@ -0,0 +1,63 @@
+import unittest                      #导入模块
+import HTMLTestRunnerNew       #导入测试报告模块
+from ddt import  data,ddt
+from jiekou.用例封装 import DoExcsl #导入类
+from jiekou.接口日志 import Mylog
+from Common import  project_path11  #导入用例的绝对路径
+from jiekou.充值接口封装 import jikoufengzhuang
+#获取测试用例
+test_Register = DoExcsl(project_path11.conf_path,"withdraw").recharge_test()
+#实例化logging
+logger = Mylog()
+COOKIES = None  #设置初始值
+# print(test_Register)
+@ddt#装饰用例
+class TestHttpRequest(unittest.TestCase):  #定义一个类
+    def setUp(self):
+        self.t = DoExcsl(project_path11.conf_path,"racharge")
+
+        print("开始执行用例了！")
+    @data(*test_Register)         #准备测试工作/测试环境
+    def test_001(self,L):
+            global TesReult  # 定义全局变量
+            global COOKIES   #声明一个cookie
+            print("---------------")
+            logger.info("********")
+            logger.info("正在执行{}模块第{}条用例：{}".format(L["Module"],L["case_id"],L["Title"]))
+            logger.info("请求的数据是：{}".format(L["ExpectedResult"]))
+            #执行http请求
+            res = jikoufengzhuang(L["URl"],param=eval(L["Params"])).http_requests(L["Method"],cookies=COOKIES)  #实际结果
+            print("用例执行的结果是：{}".format(res.text))
+            if res.cookies:  #判断请求的cookies是否为空  不为空就是ture
+                COOKIES = res.cookies
+            try:
+                self.assertEqual(eval(L["ExpectedResult"])["code"],res.json()["code"])
+                #根据状态码可以解决不好判断的情况
+                # self.assertEqual(res.json()["code"],eval(L("ExpectedResult")["code"]))
+                TesReult = "PASS"               #如果不报错，则结果为PASS
+            except AssertionError as e:         #对比实际结果与预期结果
+                logger.info("http请求测试用例出错了，错误是：".format(e))
+                print("实际结果与预期结果不一致！")
+                TesReult = "Fail"               #如果报错，则结果为Feil
+                print("异常的报错为：".format(e))
+                raise e
+
+
+            finally:  #执行完毕后最终都要写在Excel中
+                #这里利用了用例的ID与用例刚好是加一的关系
+                logger.info("******开始写入数据******")
+                self.t.write_excel(L["case_id"]+1, 9, TesReult)
+                self.t.write_excel(L["case_id"]+1, 8, res.text)  #写入实际结果
+                logger.info("******结束写入数据******")
+
+suite = unittest.TestSuite()  #测试套件
+loader = unittest.TestLoader()#用来加载用例 加载器
+suite.addTest(loader.loadTestsFromTestCase(TestHttpRequest))  #运行Testadd类下面的所有用例
+
+with open("text.html","wb") as file:  #创建一个文件存储测试报告
+      runner = HTMLTestRunnerNew.HTMLTestRunner(stream=file,
+                                                verbosity=2,
+                                                title="接口自动化测试报告",
+                                                description="2019.3.10测试报告",
+                                                tester="路人")
+      runner.run(suite) #执行测试套间里面的用例
